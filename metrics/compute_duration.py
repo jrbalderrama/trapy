0d227083a108d133f8566f8e81bddc8c5b0c667e
Javier Rojas Balderrama <javier.rojas-balderrama@univ-rennes1.fr>
2017-03-28 16:55:38 +0200

    fix compute duration function

--- metrics/compute_duration.py
+++ metrics/compute_duration.py
@@ -6,10 +6,8 @@ def compute_duration(data, frequency):
    
     :param data: kinematic rotation matrix
     :type data: numpy.ndarray
-    :param frequency: acquisition frequency
+    :param frequency: acquisition frequency (Hz)
     :return: total duration of the task
     """
-
-    return (data[1:, 0].shape[0]) * frequency
-
-
+    # all rows but one of the first column
+    return (data[1:, 0].shape[0]) / frequency
--- metrics/tests/test_compute_duration.py
+++ metrics/tests/test_compute_duration.py
@@ -1,22 +1,20 @@
-from unittest import TestCase
 import metrics
-# alternatively:
-# from metrics import compute_duration
 import numpy as np
-
 import os
 from os import path
+from unittest import TestCase
 
 
 class TestComputeDuration(TestCase):
     def setUp(self):
         cwd = os.getcwd()
         filename = path.join(cwd, "data", "data.csv")
+        # obtain raw data from file
         self.data = np.loadtxt(filename, delimiter=',')
-        # converting dummy data to a numpy.ndarray
+        # converting raw data to a numpy.ndarray
         self.data = np.reshape(self.data, (self.data.shape[0], 1))
 
     def test_compute_duration(self):
-        frequency = 0.01
-        result = metrics.compute_duration(self.data, frequency)
-        self.assertEqual(result, 1)
+        expected = 1
+        result = metrics.compute_duration(self.data, 100)
+        self.assertEqual(result, expected)
