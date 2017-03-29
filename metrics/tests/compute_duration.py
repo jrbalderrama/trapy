6f2895040b74c9934a463fdab0d5dec8842fffab
Javier Rojas Balderrama <javier.rojas-balderrama@univ-rennes1.fr>
2017-03-29 14:08:34 +0200

    replace generator function

--- metrics/tests/compute_duration.py
+++ metrics/tests/compute_duration.py
@@ -3,7 +3,8 @@ import numpy as np
 
 
 def test_compute_duration():
-    data = np.linspace(0, 1, 101)
-    data = np.reshape(data, (data.shape[0], 1))
+    urange = np.arange(0, 1.01, 0.01)
+    # converting data to a numpy.ndarray
+    data = np.reshape(urange, (urange.shape[0], 1))
     result = metrics.compute_duration(data, 100)
     assert result == 1
