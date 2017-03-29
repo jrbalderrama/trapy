db1771d5d18cb2dbc2576b8635b05694372c5ddf
Javier Rojas Balderrama <javier.rojas-balderrama@univ-rennes1.fr>
2017-03-29 13:43:05 +0200

    add test without file dependecy

--- metrics/tests/compute_duration.py
+++ metrics/tests/compute_duration.py
@@ -1,19 +1,9 @@
 import metrics
 import numpy as np
-from os import path
-import os
-
-
-def read_data():
-    cwd = os.getcwd()
-    filename = path.join(cwd, "data", "data.csv")
-    # obtain raw data from file
-    data = np.loadtxt(filename, delimiter=',')
-    # converting raw data to a numpy.ndarray
-    return np.reshape(data, (data.shape[0], 1))
 
 
 def test_compute_duration():
-    data = read_data()
+    data = np.linspace(0, 1, 101)
+    data = np.reshape(data, (data.shape[0], 1))
     result = metrics.compute_duration(data, 100)
     assert result == 1
